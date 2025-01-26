333fd0348cd66addbb4f26406a818650eac41519
dev2024 <oleg.sitnikov@icloud.com>
2025-01-26 17:40:52 +0300

    Remove SSL

--- wrapper.sh
+++ wrapper.sh
@@ -10,29 +10,7 @@ if [ -z "$PGDATA" ]; then
 fi
 
 # Set up needed variables
-SSL_DIR="/var/lib/postgresql/data/certs"
-INIT_SSL_SCRIPT="/docker-entrypoint-initdb.d/init-ssl.sh"
 POSTGRES_CONF_FILE="$PGDATA/postgresql.conf"
-
-# Regenerate if the certificate is not a x509v3 certificate
-if [ -f "$SSL_DIR/server.crt" ] && ! openssl x509 -noout -text -in "$SSL_DIR/server.crt" | grep -q "DNS:localhost"; then
-  echo "Did not find a x509v3 certificate, regenerating certificates..."
-  bash "$INIT_SSL_SCRIPT"
-fi
-
-# Regenerate if the certificate has expired or will expire
-# 2592000 seconds = 30 days
-if [ -f "$SSL_DIR/server.crt" ] && ! openssl x509 -checkend 2592000 -noout -in "$SSL_DIR/server.crt"; then
-  echo "Certificate has or will expire soon, regenerating certificates..."
-  bash "$INIT_SSL_SCRIPT"
-fi
-
-# Generate a certificate if the database was initialized but is missing a certificate
-# Useful when going from the base postgres image to this ssl image
-if [ -f "$POSTGRES_CONF_FILE" ] && [ ! -f "$SSL_DIR/server.crt" ]; then
-  echo "Database initialized without certificate, generating certificates..."
-  bash "$INIT_SSL_SCRIPT"
-fi
 
 # unset PGHOST to force psql to use Unix socket path
 # this is specific to Railway and allows
